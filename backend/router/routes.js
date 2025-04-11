523999da67ba54a491c1478422a3f4299c3538fe
nanduaji <nanduaji64@gmail.com>
2025-04-11 20:52:04 +0530

    Organize route definitions by adding section comments for user, admin, and service provider routes

--- backend/router/routes.js
+++ backend/router/routes.js
@@ -6,6 +6,7 @@ const authMiddleware = require("../middleware/authMiddleware");
 
 const routes = require("express").Router();
 
+// USER ROUTES
 routes.post("/addUser", addUser); 
 routes.post("/userLogin", userLogin);
 routes.post("/getUsers", authMiddleware, getUsers); 
@@ -14,10 +15,12 @@ routes.post("/bookService", authMiddleware, bookService);
 routes.post("/getServices", authMiddleware, getServices);
 routes.post("/getStatus/:id", authMiddleware, getStatus);
 
+
+// ADMIN ROUTES
 routes.post("/addAdmin", addAdmin);
 routes.post("/adminLogin", adminLogin);
 
-
+// SERVICE PROVIDER ROUTES
 routes.post("/addServiceProvider", authMiddleware, addServiceProvider);
 routes.post("/serviceProviderLogin", serviceProviderLogin);
 module.exports = routes;
