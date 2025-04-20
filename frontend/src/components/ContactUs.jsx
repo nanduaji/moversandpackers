f33506764bb32896a6a1e863ad05d8212d69e68c
nanduaji <nanduaji64@gmail.com>
2025-04-20 14:52:29 +0530

    Update map iframe source to display location in Kollam, Kerala

--- frontend/src/components/ContactUs.jsx
+++ frontend/src/components/ContactUs.jsx
@@ -20,14 +20,15 @@ function ContactUs() {
 
         <div className={styles.mapSection}>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.3913770281523!2d90.40304931538406!3d23.8052167925285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c7a8359c1cbb%3A0x4a6c0d71ec61f7e9!2sDhaka!5e0!3m2!1sen!2sbd!4v1611072543144!5m2!1sen!2sbd"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3930.3658262445983!2d76.59206881480122!3d8.893211093616934!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05fdd5ae00d9a1%3A0x7ef4d3c6f3a8cb6b!2sKollam%2C%20Kerala!5e0!3m2!1sen!2sin!4v1713634290900!5m2!1sen!2sin"
             width="100%"
             height="100%"
             style={{ border: 0 }}
             allowFullScreen=""
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+          />
+
         </div>
       </div>
     </div>
